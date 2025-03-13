1a1d65568fd05212f03bb7adf5b12bfff13ff19c
Espadv69 <aitorjob69@gmail.com>
2025-03-13 16:07:15 +0100

    Implement LiExplanation component with props for step number, description, and image

--- LimitedCounter/src/components/Explanation/LiExplanation.jsx
+++ LimitedCounter/src/components/Explanation/LiExplanation.jsx
@@ -1,3 +1,12 @@
-const LiExplanation = () => {}
+const LiExplanation = ({ stepNumber, description, imgUrl, imgAlt }) => {
+  return (
+    <li className="li__explanation">
+      <p>
+        <strong>{stepNumber}.</strong> {description}
+        <img src={imgUrl} alt={imgAlt} />
+      </p>
+    </li>
+  )
+}
 
 export default LiExplanation
