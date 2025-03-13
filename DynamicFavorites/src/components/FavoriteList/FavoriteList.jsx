eca04fe1be003a86566079a37f353fad6adb9381
Espadv69 <aitorjob69@gmail.com>
2025-03-13 18:15:04 +0100

    Add initial data for FavoriteList component

--- DynamicFavorites/src/components/FavoriteList/FavoriteList.jsx
+++ DynamicFavorites/src/components/FavoriteList/FavoriteList.jsx
@@ -1,5 +1,11 @@
 import './FavoriteList.css'
 
+// Initial data
+const INITIAL_FAVORITES = ['React', 'Angular', 'Vue']
+const INITIAL_LIST = [...INITIAL_FAVORITES, 'Svelte', 'Ember', 'Preact']
+
+console.log(INITIAL_FAVORITES, INITIAL_LIST)
+
 const FavoriteList = () => {}
 
 export default FavoriteList
