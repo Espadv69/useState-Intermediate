80400da093b3056e2c870009e0c241515a4fdf64
Espadv69 <aitorjob69@gmail.com>
2025-03-13 15:45:52 +0100

    Add input field and button for setting limit in Counter component

--- LimitedCounter/src/components/Counter/Counter.jsx
+++ LimitedCounter/src/components/Counter/Counter.jsx
@@ -31,6 +31,26 @@ const Counter = () => {
   const setLimitValue = (value) => {
     setLimit(value)
   }
+
+  return (
+    <section className="counter">
+      <h1>Limited Counter</h1>
+
+      {count}
+      <div className="counter__container">
+        <div className="counter__container--buttons">
+          <button onClick={increment}>Increment</button>
+          <button onClick={decrement}>Decrement</button>
+          <button onClick={reset}>Reset</button>
+        </div>
+
+        <div className="counter__container--input">
+          <input type="number" value={limit} placeholder="Set limit" />
+          <button onClick={setLimitValue(limit)}>Set Limit</button>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default Counter
