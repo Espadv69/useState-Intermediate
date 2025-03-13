ea867cea4c3d029684b9c1fc70d3cbdac3807847
Espadv69 <aitorjob69@gmail.com>
2025-03-13 16:03:14 +0100

    Add rendering logic to Explanation component

--- LimitedCounter/src/components/Explanation/Explanation.jsx
+++ LimitedCounter/src/components/Explanation/Explanation.jsx
@@ -1,5 +1,12 @@
+import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <ol className="ol__explanation"></ol>
+    </section>
+  )
+}
 
 export default Explanation
