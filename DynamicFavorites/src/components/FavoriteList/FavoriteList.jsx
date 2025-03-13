e23f8cf05424a966fae06a4f800c3d1f35ddda56
Espadv69 <aitorjob69@gmail.com>
2025-03-13 18:17:12 +0100

    Initialize state in FavoriteList component with default favorites and list

--- DynamicFavorites/src/components/FavoriteList/FavoriteList.jsx
+++ DynamicFavorites/src/components/FavoriteList/FavoriteList.jsx
@@ -1,11 +1,14 @@
+import { useState } from 'react'
 import './FavoriteList.css'
 
 // Initial data
 const INITIAL_FAVORITES = ['React', 'Angular', 'Vue']
 const INITIAL_LIST = [...INITIAL_FAVORITES, 'Svelte', 'Ember', 'Preact']
 
-console.log(INITIAL_FAVORITES, INITIAL_LIST)
-
-const FavoriteList = () => {}
+const FavoriteList = () => {
+  // State with initial data
+  const [favorites, setFavorites] = useState(INITIAL_FAVORITES)
+  const [list, setList] = useState(INITIAL_LIST)
+}
 
 export default FavoriteList
