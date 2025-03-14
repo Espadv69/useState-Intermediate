b400473865246e3f5608f0e48572a8b1f6aaf55b
Espadv69 <aitorjob69@gmail.com>
2025-03-14 18:29:46 +0100

    Remove initial data constants from FavoriteList component

--- DynamicFavorites/src/components/FavoriteList/FavoriteList.jsx
+++ DynamicFavorites/src/components/FavoriteList/FavoriteList.jsx
@@ -1,10 +1,6 @@
 import { useState } from 'react'
 import './FavoriteList.css'
 
-// Initial data
-const INITIAL_FAVORITES = ['React', 'Angular', 'Vue']
-const INITIAL_LIST = [...INITIAL_FAVORITES, 'Svelte', 'Ember', 'Preact']
-
 const FavoriteList = () => {
   // State with initial data
   const [favorites, setFavorites] = useState(INITIAL_FAVORITES)
