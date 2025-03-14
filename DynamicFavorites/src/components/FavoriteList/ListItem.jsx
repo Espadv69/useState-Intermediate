14b4032aeda4311556c6b35b1bcd5f584493184a
Espadv69 <aitorjob69@gmail.com>
2025-03-14 18:42:44 +0100

    Implement rendering and add button functionality in ListItem component

--- DynamicFavorites/src/components/FavoriteList/ListItem.jsx
+++ DynamicFavorites/src/components/FavoriteList/ListItem.jsx
@@ -1,3 +1,12 @@
-const ListItem = ({ item, onAddFavorite }) => {}
+const ListItem = ({ item, onAddFavorite }) => {
+  return (
+    <li className="list__li" key={item}>
+      {item}{' '}
+      <button className="list__li-button" onClick={() => onAddFavorite(item)}>
+        Add
+      </button>
+    </li>
+  )
+}
 
 export default ListItem
