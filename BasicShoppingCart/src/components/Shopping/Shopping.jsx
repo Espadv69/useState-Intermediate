6effdccf078a30de1bf1faf2e6c03b5ff2b43bf4
Espadv69 <aitorjob69@gmail.com>
2025-03-14 20:28:10 +0100

    Initialize state for cart and total price in Shopping component

--- BasicShoppingCart/src/components/Shopping/Shopping.jsx
+++ BasicShoppingCart/src/components/Shopping/Shopping.jsx
@@ -1,5 +1,13 @@
+import { useState } from 'react'
+import { data } from '../../data/data.js'
+
 import './Shopping.css'
 
-const Shopping = () => {}
+const Shopping = () => {
+  // State for the cart
+  const [cart, setCart] = useState([])
+  // State for the total price
+  const [total, setTotal] = useState(0)
+}
 
 export default Shopping
