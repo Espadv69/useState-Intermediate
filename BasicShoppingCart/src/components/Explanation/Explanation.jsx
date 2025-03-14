6f1bb16e3c16647e36b1860db2f9a48ac6ca8d23
Espadv69 <aitorjob69@gmail.com>
2025-03-14 20:59:38 +0100

    Implement Explanation component to render LiExplanation with step details and image

--- BasicShoppingCart/src/components/Explanation/Explanation.jsx
+++ BasicShoppingCart/src/components/Explanation/Explanation.jsx
@@ -1,3 +1,18 @@
-const Explanation = () => {}
+import LiExplanation from './LiExplanation'
+
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <ul className="ul__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Create the states for the cart and the total price."
+          imgUrl="/images/initHooks.png"
+          imgAlt="Code snippet for initializing the states"
+        />
+      </ul>
+    </section>
+  )
+}
 
 export default Explanation
