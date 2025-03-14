91b095c5b1fe894c3aa1228dabb52fb3b0516306
Espadv69 <aitorjob69@gmail.com>
2025-03-14 20:49:26 +0100

    Integrate Shopping component into App for enhanced application structure

--- BasicShoppingCart/src/App.jsx
+++ BasicShoppingCart/src/App.jsx
@@ -1,3 +1,11 @@
-const App = () => {}
+import Shopping from './components/Shopping/Shopping'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Shopping />
+    </div>
+  )
+}
 
 export default App
