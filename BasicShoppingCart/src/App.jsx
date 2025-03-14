e7b73ebb1e18642f76ae94de7de1fb6f7f8105f9
Espadv69 <aitorjob69@gmail.com>
2025-03-14 21:02:10 +0100

    Add Explanation component to App for enhanced user guidance

--- BasicShoppingCart/src/App.jsx
+++ BasicShoppingCart/src/App.jsx
@@ -1,9 +1,11 @@
 import Shopping from './components/Shopping/Shopping'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <Shopping />
+      <Explanation />
     </div>
   )
 }
