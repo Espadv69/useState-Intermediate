b01b4680bbf0a798adeac1b3190b25e39c17f84d
Espadv69 <aitorjob69@gmail.com>
2025-03-14 18:32:38 +0100

    Refactor FavoriteItem component to use 'item' prop and implement remove functionality

--- DynamicFavorites/src/components/FavoriteList/FavoriteItem.jsx
+++ DynamicFavorites/src/components/FavoriteList/FavoriteItem.jsx
@@ -1,3 +1,12 @@
-const FavoriteItem = ({ favorite, onRemove }) => {}
+const FavoriteItem = ({ item, onRemove }) => {
+  return (
+    <li className="favorite__li" key={item}>
+      {item}{' '}
+      <button className="favorite__li-button" onClick={() => onRemove(item)}>
+        Remove
+      </button>
+    </li>
+  )
+}
 
 export default FavoriteItem
