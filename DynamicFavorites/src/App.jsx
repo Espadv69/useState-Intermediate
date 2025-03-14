f1e9a4a5ea6992e130bc5d4363a267a0168b1a7b
Espadv69 <aitorjob69@gmail.com>
2025-03-14 15:56:27 +0100

    Add Explanation component to App for usage instructions

--- DynamicFavorites/src/App.jsx
+++ DynamicFavorites/src/App.jsx
@@ -1,9 +1,11 @@
 import FavoriteList from './components/FavoriteList/FavoriteList'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <FavoriteList />
+      <Explanation />
     </div>
   )
 }
