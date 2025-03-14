1b631baff062e70e658ff99616b9c9c7a1f920e1
Espadv69 <aitorjob69@gmail.com>
2025-03-14 20:24:11 +0100

    Add product descriptions and new product entry to data.js in BasicShoppingCart

--- BasicShoppingCart/src/data/data.js
+++ BasicShoppingCart/src/data/data.js
@@ -2,7 +2,15 @@ export const data = [
   {
     id: 1,
     name: 'T-shirt',
+    description: 'This is a t-shirt',
     price: 10,
+    image: 'https://via.placeholder.com/150',
+  },
+  {
+    id: 2,
+    name: 'Pants',
+    description: 'This is a pants',
+    price: 20,
     image: 'https://via.placeholder.com/150',
   },
 ]
