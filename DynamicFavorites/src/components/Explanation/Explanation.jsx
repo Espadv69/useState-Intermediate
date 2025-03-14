9c87d2ec2684dcb20ac44a1de83edcfa34e63b59
Espadv69 <aitorjob69@gmail.com>
2025-03-14 15:50:10 +0100

    Implement Explanation component to render usage instructions with LiExplanation

--- DynamicFavorites/src/components/Explanation/Explanation.jsx
+++ DynamicFavorites/src/components/Explanation/Explanation.jsx
@@ -1,5 +1,20 @@
+import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <h2 className="explanation__title">How to use</h2>
+      <ul className="ul__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Initializing data in JavaScript using constants and arrays."
+          imgUrl="/images/initialData.png"
+          imgAlt="Code snippet with initial data"
+        />
+      </ul>
+    </section>
+  )
+}
 
 export default Explanation
