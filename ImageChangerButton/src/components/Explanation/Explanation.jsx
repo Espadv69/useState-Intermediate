7c1e82b3e71819a5a27fa46d6415c5ae9196304a
Espadv69 <aitorjob69@gmail.com>
2025-03-15 12:55:54 +0100

    Implement Explanation component to render LiExplanation with initial props

--- ImageChangerButton/src/components/Explanation/Explanation.jsx
+++ ImageChangerButton/src/components/Explanation/Explanation.jsx
@@ -1,5 +1,19 @@
+import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <ul className="ul__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Init hook to set the initial state of the image"
+          imgUrl="/images/initHook.png"
+          alt="Code snippet of the init hook"
+        />
+      </ul>
+    </section>
+  )
+}
 
 export default Explanation
