a0f4969f0ec5e41af2d57fea6c4183917cb3544e
Espadv69 <aitorjob69@gmail.com>
2025-03-15 19:34:18 +0100

    Enhance LiExplanation component to accept props for step number, description, and image details

--- RandomColorGenerator/src/components/Explanation/LiExplanation.jsx
+++ RandomColorGenerator/src/components/Explanation/LiExplanation.jsx
@@ -1,3 +1,12 @@
-const LiExplanation = () => {}
+const LiExplanation = ({ stepNumber, description, imgUrl, imgAlt }) => {
+  return (
+    <li className="li__explanation">
+      <p>
+        <strong>{stepNumber}.</strong> {description}
+      </p>
+      <img src={imgUrl} alt={imgAlt} />
+    </li>
+  )
+}
 
 export default LiExplanation
