9098d950494d860e551977dac36bd5d23b2d8e59
Espadv69 <aitorjob69@gmail.com>
2025-03-15 13:05:47 +0100

    Add first step to Explanation component for importing IMAGES array

--- ImageChangerButton/src/components/Explanation/Explanation.jsx
+++ ImageChangerButton/src/components/Explanation/Explanation.jsx
@@ -7,6 +7,13 @@ const Explanation = () => {
       <h2 className="explanation__title">Explanation</h2>
       <ul className="ul__explanation">
         <LiExplanation
+          stepNumber={1}
+          description="Import our IMAGES array"
+          imgUrl="/images/importImages.png"
+          alt="Code snippet of importing the IMAGES array"
+        />
+
+        <LiExplanation
           stepNumber={2}
           description="Init hook to set the initial state of the image"
           imgUrl="/images/initHook.png"
