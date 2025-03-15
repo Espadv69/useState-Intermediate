85b9027327e5ae77e808553c2348f21d720ff600
Espadv69 <aitorjob69@gmail.com>
2025-03-15 12:43:05 +0100

    Add state management for current image index in ImageChanger component

--- ImageChangerButton/src/components/ImageChanger/ImageChanger.jsx
+++ ImageChangerButton/src/components/ImageChanger/ImageChanger.jsx
@@ -3,6 +3,9 @@ import { Images } from '../../data/images.js'
 
 import './ImageChanger.css'
 
-const ImageChanger = () => {}
+const ImageChanger = () => {
+  // State to keep track of the current image index
+  const [imageIndex, setImageIndex] = useState(0)
+}
 
 export default ImageChanger
