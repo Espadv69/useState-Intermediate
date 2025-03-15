2573fed2001390c5515c0fdefd0c5b00e32f576d
Espadv69 <aitorjob69@gmail.com>
2025-03-15 12:56:24 +0100

    Add Explanation component to App for enhanced user guidance

--- ImageChangerButton/src/App.jsx
+++ ImageChangerButton/src/App.jsx
@@ -1,9 +1,11 @@
-import ImageChanger from "./components/ImageChanger/ImageChanger"
+import ImageChanger from './components/ImageChanger/ImageChanger'
+import Explanation from './components/Explanation/Explanation'
 
 const App = () => {
   return (
     <div className="app">
       <ImageChanger />
+      <Explanation />
     </div>
   )
 }
