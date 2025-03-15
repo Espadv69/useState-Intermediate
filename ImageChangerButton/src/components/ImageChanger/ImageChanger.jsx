7d69744166d542494ebed5adc9f58ad603e038f8
Espadv69 <aitorjob69@gmail.com>
2025-03-15 12:44:44 +0100

    Add image change functionality to ImageChanger component

--- ImageChangerButton/src/components/ImageChanger/ImageChanger.jsx
+++ ImageChangerButton/src/components/ImageChanger/ImageChanger.jsx
@@ -1,11 +1,16 @@
 import { useState } from 'react'
-import { Images } from '../../data/images.js'
+import { IMAGES } from '../../data/images.js'
 
 import './ImageChanger.css'
 
 const ImageChanger = () => {
   // State to keep track of the current image index
   const [imageIndex, setImageIndex] = useState(0)
+
+  // Function to change the image
+  const changeImage = () => {
+    setImageIndex((imageIndex + 1) % IMAGES.length)
+  }
 }
 
 export default ImageChanger
