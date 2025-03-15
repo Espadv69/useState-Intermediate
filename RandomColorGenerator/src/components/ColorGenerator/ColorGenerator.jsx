c0b8fac4898b40f3ff40b6dec4b367056b4f9257
Espadv69 <aitorjob69@gmail.com>
2025-03-15 19:14:03 +0100

    Add state management for color index and current color in ColorGenerator component

--- RandomColorGenerator/src/components/ColorGenerator/ColorGenerator.jsx
+++ RandomColorGenerator/src/components/ColorGenerator/ColorGenerator.jsx
@@ -3,6 +3,11 @@ import { COLORS } from '../../data/colors'
 
 import './ColorGenerator.css'
 
-const ColorGenerator = () => {}
+const ColorGenerator = () => {
+  // State to keep track of the current color index
+  const [colorIndex, setColorIndex] = useState(0)
+  // State to keep track of the current color
+  const [color, setColor] = useState(COLORS[colorIndex])
+}
 
 export default ColorGenerator
