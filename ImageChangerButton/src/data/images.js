453103c6cb3d5b065e6d915f98a8bf6093483e08
Espadv69 <aitorjob69@gmail.com>
2025-03-15 12:39:24 +0100

    Add images data for ImageChangerButton component

---
+++ ImageChangerButton/src/data/images.js
@@ -0,0 +1,14 @@
+export const IMAGES = [
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/001.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/021.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/041.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/065.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/101.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/201.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/033.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/055.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/093.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/036.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/012.png',
+  'https://assets.pokemon.com/assets/cms2/img/pokedex/full/087.png',
+]
