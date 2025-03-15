0b0f03f4abb18f63f9a34f3f2e9d57474935f827
Espadv69 <aitorjob69@gmail.com>
2025-03-15 19:36:17 +0100

    Implement Explanation component to guide users on importing the COLORS array

--- RandomColorGenerator/src/components/Explanation/Explanation.jsx
+++ RandomColorGenerator/src/components/Explanation/Explanation.jsx
@@ -1,6 +1,21 @@
 import LiExplanation from './LiExplanation'
 import './Explanation.css'
 
-const Explanation = () => {}
+const Explanation = () => {
+  return (
+    <section className="explanation">
+      <h1>How to use</h1>
+
+      <ul className="ul__explanation">
+        <LiExplanation
+          stepNumber={1}
+          description="Import COLORS array from the colors.js file."
+          imgUrl="/images/importColors.png"
+          imgAlt="Code snippet showing how to import COLORS array from the colors.js file."
+        />
+      </ul>
+    </section>
+  )
+}
 
 export default Explanation
