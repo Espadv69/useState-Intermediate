3ad58b46f69e5e5f1ab21c610f83cae551f9593b
Espadv69 <aitorjob69@gmail.com>
2025-03-15 13:04:57 +0100

    Add fifth step to Explanation component for displaying HTML elements

--- ImageChangerButton/src/components/Explanation/Explanation.jsx
+++ ImageChangerButton/src/components/Explanation/Explanation.jsx
@@ -7,24 +7,31 @@ const Explanation = () => {
       <h2 className="explanation__title">Explanation</h2>
       <ul className="ul__explanation">
         <LiExplanation
-          stepNumber={1}
+          stepNumber={2}
           description="Init hook to set the initial state of the image"
           imgUrl="/images/initHook.png"
           alt="Code snippet of the init hook"
         />
 
         <LiExplanation
-          stepNumber={2}
+          stepNumber={3}
           description="Function to change the image"
           imgUrl="/images/changeImageFunction.png"
           alt="Code snippet of the changeImage function"
         />
 
         <LiExplanation
-          stepNumber={3}
+          stepNumber={4}
           description="Get the current image"
           imgUrl="/images/getCurrentImage.png"
           alt="Code snippet of getting the current image"
+        />
+
+        <LiExplanation
+          stepNumber={5}
+          description="Display HTML elements"
+          imgUrl="/images/htmlStructure.png"
+          alt="Code snippet of displaying HTML elements"
         />
       </ul>
     </section>
